dc68bcde97d7716335cd676398486cbdf82f1f8a
dtp06077 <dtp06077@gmail.com>
2024-05-03 16:38:38 +0900

    영속성 컨텍스트 영속 엔티티의 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -29,10 +29,15 @@ public class JpaMain {
 
             //1차 캐시에서 엔티티를 가져오므로 select 쿼리가 나가지 않음.
             Member findMember = em.find(Member.class, 6L);
+            Member findMember2 = em.find(Member.class, 6L);
+
             System.out.println("findMember.id = " + findMember.getId());
             System.out.println("findMember.name = " + findMember.getName());
 
-           List<Member> result = em.createQuery("select m from Member as m", Member.class)
+            //영속 엔티티의 동일성 보장
+            System.out.println("result = " + (findMember2==findMember));
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
                    .setFirstResult(2)
                    .setMaxResults(4)
                    .getResultList();
