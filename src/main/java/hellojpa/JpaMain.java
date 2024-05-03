92cc8b1df6ba1abe456c963a9bd8a84d0e00eaac
dtp06077 <dtp06077@gmail.com>
2024-05-03 16:21:33 +0900

    영속성 컨텍스트 비영속, 영속 상태 확인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,15 +15,24 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //비영속
+            Member member = new Member();
+            member.setId(6L);
+            member.setName("memberA");
+
+            //영속
+            System.out.println("==BEFORE==");
+            //영속성 컨텍스트에 persist해도 바로 db에 반영되지 않음.
+            //AFTER가 출력된 후에 쿼리가 나감.
+            em.persist(member);
+            System.out.println("==AFTER==");
+
            List<Member> result = em.createQuery("select m from Member as m", Member.class)
                    .setFirstResult(2)
                    .setMaxResults(4)
                    .getResultList();
 
-           for(Member member : result) {
-               System.out.println("Member.name = "+member.getName());
-           }
-
+            //커밋 시점에 영속성 컨텍스트에 영속된 엔티티들이 db에 반영
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
