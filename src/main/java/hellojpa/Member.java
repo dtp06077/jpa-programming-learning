97ad62fdfe118414bab6f67fbd2b72d59186a594
dtp06077 <dtp06077@gmail.com>
2024-05-05 14:52:40 +0900

    DDL 생성 기능

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,16 +1,19 @@
 package hellojpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@Entity @Getter @Setter
-
+@Entity(name = "Member") //JPA에서 사용할 엔티티 이름을 지정
+@Getter @Setter
+@Table(name = "MEMBER") //매핑할 테이블 이름
 public class Member {
 
     @Id
     private Long id;
+    @Column(unique = true, length = 10)
     private String name;
 }
