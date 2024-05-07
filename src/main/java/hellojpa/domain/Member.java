15501d25ddb81d301ebb8f64179662779fcad25d
dtp06077 <dtp06077@gmail.com>
2024-05-08 01:43:11 +0900

    Feat: Member 엔티티 생성

---
+++ src/main/java/hellojpa/domain/Member.java
@@ -0,0 +1,17 @@
+package hellojpa.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+}
