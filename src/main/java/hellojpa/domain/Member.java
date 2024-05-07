f71be46392cb1acff789bc6f13f8647575f774eb
dtp06077 <dtp06077@gmail.com>
2024-05-08 01:45:33 +0900

    Feat: Member 엔티티 생성

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
+@Entity @Getter @Setter
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
