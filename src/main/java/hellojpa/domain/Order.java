cdc209c585ac85adeaa1f3c08f3841aeb88a6977
dtp06077 <dtp06077@gmail.com>
2024-05-08 01:49:37 +0900

    Feat: Order 엔티티 생성

--- src/main/java/hellojpa/domain/Order.java
+++ src/main/java/hellojpa/domain/Order.java
@@ -1,4 +1,21 @@
 package hellojpa.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity @Getter @Setter
 public class Order {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long memberId;
+
+    private LocalDate orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
 }
