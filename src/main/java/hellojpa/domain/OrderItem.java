a8428f7faf95dd7c78d4c8c719dc8ee386fd026b
dtp06077 <dtp06077@gmail.com>
2024-05-08 02:01:09 +0900

    Feat: OrderItem 엔티티 생성

--- src/main/java/hellojpa/domain/OrderItem.java
+++ src/main/java/hellojpa/domain/OrderItem.java
@@ -1,4 +1,25 @@
 package hellojpa.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "ORDER_ITEM")
 public class OrderItem {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    private int orderPrice;
+
+    private int count;
 }
