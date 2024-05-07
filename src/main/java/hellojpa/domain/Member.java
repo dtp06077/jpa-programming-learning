64d3e3cc4f0cd00fadb5c2f1e4d0d46c59979054
dtp06077 <dtp06077@gmail.com>
2024-05-08 01:54:10 +0900

    Fix: Member 엔티티 id 컬럼 명 변경

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -1,9 +1,6 @@
 package hellojpa.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,9 +8,14 @@ import lombok.Setter;
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "MEMBER_ID")
     private Long id;
+
     private String name;
+
     private String city;
+
     private String street;
+
     private String zipcode;
 }
