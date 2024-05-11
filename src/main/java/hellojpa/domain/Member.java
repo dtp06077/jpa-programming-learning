85d2399cb890dfd05b772e54ddce6fc8a09fd656
dtp06077 <dtp06077@gmail.com>
2024-05-12 01:42:52 +0900

    Feat: 연관관계 매핑

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity @Getter @Setter
 public class Member {
 
@@ -18,4 +21,7 @@ public class Member {
     private String street;
 
     private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 }
--- src/main/java/hellojpa/domain/Order.java
+++ src/main/java/hellojpa/domain/Order.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity @Getter @Setter
 @Table(name = "ORDERS")
@@ -14,12 +16,24 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
 
     private LocalDate orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    //연관관계 편의 메서드
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 }
