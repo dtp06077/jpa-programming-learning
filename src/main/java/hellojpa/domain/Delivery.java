6b875995c7427c7014262da56164fab31d5da2bc
dtp06077 <dtp06077@gmail.com>
2024-05-23 17:31:03 +0900

    Feat: 일대일, 다대다 연관관계 매핑

---
+++ src/main/java/hellojpa/domain/Delivery.java
@@ -0,0 +1,24 @@
+package hellojpa.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "DELIVERY_ID")
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+}
--- src/main/java/hellojpa/domain/Item.java
+++ src/main/java/hellojpa/domain/Item.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter @Setter
 public class Item {
@@ -16,4 +19,7 @@ public class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
