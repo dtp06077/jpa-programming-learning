68156dec37553741b3688b52115ddc130216085d
dtp06077 <dtp06077@gmail.com>
2024-06-05 17:54:53 +0900

    임베디드 타입의 불변 객체 설정

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -3,11 +3,14 @@ package hellojpa;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
-//임베디드 타입
+// 임베디드 타입
+// 임베디드 타입은 객체 타입이므로 실제 인스턴스인 값을 공유하면 문제 발생.
+// 따라서 인스턴스 값들을 final 로 정의하거나, set 메서드를 제거하여 수정할 수 없게하여
+// 생성 시점 이후 절대 값을 변경할 수 없는 불변 객체로 설계.
 @Embeddable
 @RequiredArgsConstructor
 @NoArgsConstructor(force = true)
-@Getter @Setter
+@Getter
 public class Address {
     private final String city;
     private final String street;
--- src/main/java/hellojpa/Period.java
+++ src/main/java/hellojpa/Period.java
@@ -2,13 +2,12 @@ package hellojpa;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 //임베디드 타입
 @Embeddable
-@Getter @Setter
+@Getter
 public class Period {
     private LocalDateTime startDate;
     private LocalDateTime endDate;
