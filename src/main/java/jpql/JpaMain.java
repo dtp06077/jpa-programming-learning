66a4ab7f39a9728e6acfaf07e9143bcdeefe31c3
dtp06077 <dtp06077@gmail.com>
2024-06-09 23:12:05 +0900

    TypeQuery 와 Query

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,8 +13,14 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("KimHuiSeong");
+            member.setAge(20);
             em.persist(member);
 
+            //TypeQuery : 반환 타입이 명확할 때 사용(ex Member.class)
+            TypedQuery<Member> typeQuery = em.createQuery("select m from Member m", Member.class);
+
+            //Query : 반환 타입이 명확하지 않을 때 사용(String username, int age)
+            Query query = em.createQuery("select m.username, m.age from Member m");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
