21bc628db1ee334ad922d04ff2735032ecd05933
dtp06077 <dtp06077@gmail.com>
2024-06-09 23:18:56 +0900

    결과 조회 API

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -2,6 +2,8 @@ package jpql;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -18,6 +20,14 @@ public class JpaMain {
 
             //TypeQuery : 반환 타입이 명확할 때 사용(ex Member.class)
             TypedQuery<Member> typeQuery = em.createQuery("select m from Member m", Member.class);
+
+            //결과가 하나 이상일 떄, 리스트 반환
+            //결과가 없으면 빈 리스트 반환
+            List<Member> resultList = typeQuery.getResultList();
+
+            //결과가 정확히 하나일 때, 단일 객체 반환
+            //결과가 없거나 둘 이상이면 Exception 발생
+            Member singleResult = typeQuery.getSingleResult();
 
             //Query : 반환 타입이 명확하지 않을 때 사용(String username, int age)
             Query query = em.createQuery("select m.username, m.age from Member m");
